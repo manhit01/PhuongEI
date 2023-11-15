37c7b8b6915d7b257b06323dd86edf18775cd381
manhit01 <128140274+manhit01@users.noreply.github.com>
2023-11-15 21:17:08 +0700

    Add files via upload
    PhươngEI

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật Em ';
+const titleCard = 'Tặng Người Anh IU ';
+const contentCard = 'Trong ngày sinh nhật này, anh muốn nói với em rằng em là người phụ nữ tuyệt vời nhất của anh. Anh hy vọng em sẽ luôn được hạnh phúc và thành công trong cuộc sống.';
+
+
+const giftImage = 'h1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
